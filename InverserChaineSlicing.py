303fc66e3cfd19147d9de2c6c223df2bdbf033fb
kemskevin <145323783+kemskevin@users.noreply.github.com>
2024-12-12 11:52:14 +0100

    Update InverserChaineSlicing.py

--- InverserChaineSlicing.py
+++ InverserChaineSlicing.py
@@ -1,4 +1,4 @@
-"""Inverse une chaîne de caractères."""
+"""Inversons une chaîne de caractères."""
 chaine = input("Entrez une chaîne : ")
 chaine_inversee = chaine[::-1]  # Slicing pour inverser la chaîne
 print("La chaîne inversée est :", chaine_inversee)
